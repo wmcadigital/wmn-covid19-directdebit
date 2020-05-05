96c91ad6e76cd5d6c654c6ba462c9558153f0005
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-05-05 15:52:42 +0100

    Updated step 2 to go to next step

--- src/components/Form/Step2DDRef/Step2DDRef.js
+++ src/components/Form/Step2DDRef/Step2DDRef.js
@@ -7,7 +7,7 @@ import { FormErrorContext } from 'globalState/FormErrorContext';
 import GenericError from 'components/shared/Errors/GenericError';
 import Input from 'components/shared/FormElements/Input/Input';
 
-const Step2DDRef = ({ currentStep, setCurrentStep, formRef }) => {
+const Step2DDRef = ({ setCurrentStep, formRef }) => {
   const [formState] = useContext(FormContext); // Get the state of form data from FormContext
   const [errorState, errorDispatch] = useContext(FormErrorContext); // Get the error state of form data from FormErrorContext
   const label = 'Direct Debit reference'; // Used on input and for validation
@@ -40,7 +40,7 @@ const Step2DDRef = ({ currentStep, setCurrentStep, formRef }) => {
       errorDispatch({ type: 'CONTINUE_PRESSED', payload: true }); // set continue button pressed to true so errors can show
     } else {
       errorDispatch({ type: 'CONTINUE_PRESSED', payload: false }); // Reset submit button pressed before going to next step
-      setCurrentStep(currentStep + 1); // Set to next step in form
+      setCurrentStep((c) => c + 1); // Set to next step in form
       window.scrollTo(0, 0); // Scroll to top of page
     }
   };
@@ -87,7 +87,6 @@ const Step2DDRef = ({ currentStep, setCurrentStep, formRef }) => {
 };
 
 Step2DDRef.propTypes = {
-  currentStep: PropTypes.number.isRequired,
   setCurrentStep: PropTypes.func.isRequired,
   formRef: PropTypes.oneOfType([
     // Either a function
