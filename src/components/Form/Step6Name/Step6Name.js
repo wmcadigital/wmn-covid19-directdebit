2bbd481d71e3ae83d984a39e4436bed6e3fc7917
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-05-11 08:58:52 +0100

    Step66 converted to new form

--- src/components/Form/Step6Name/Step6Name.js
+++ src/components/Form/Step6Name/Step6Name.js
@@ -1,35 +1,31 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-// Import contexts
-import { FormErrorContext } from 'globalState/FormErrorContext';
+// Import custom hooks
+import useStepLogic from 'components/Form/useStepLogic';
 // Import components
-import GenericError from 'components/shared/Errors/GenericError';
 import Input from 'components/shared/FormElements/Input/Input';
 
-const Step6Name = ({ setCurrentStep, formRef }) => {
-  const [errorState, errorDispatch] = useContext(FormErrorContext); // Get the error state of form data from FormErrorContext
+const Step6Name = ({ formRef }) => {
+  const { register, showGenericError, handleContinue } = useStepLogic(formRef);
 
-  // Goto next step on continue
-  const handleContinue = () => {
-    // If errors, then don't progress and set continue button to true(halt form and show errors)
-    if (errorState.errors.length) {
-      window.scrollTo(0, formRef.current.offsetTop); // Scroll to top of form
-      errorDispatch({ type: 'CONTINUE_PRESSED', payload: true }); // set continue button pressed to true so errors can show
-    } else {
-      errorDispatch({ type: 'CONTINUE_PRESSED', payload: false }); // Reset submit button pressed before going to next step
-      setCurrentStep((c) => c + 1); // Set to next step in form
-      window.scrollTo(0, 0); // Scroll to top of page
-    }
+  // Labels used on input and for validation
+  const fNameLabel = 'First name';
+  const lNameLabel = 'Last name';
+  // Logic used to validate fields
+  const fieldValidation = (name) => {
+    return register({ required: `${name} is required` });
   };
 
   return (
     <>
-      <p>
+      {/* Subsection */}
+      <div>
         Section 2 of 3 <h4>About you</h4>
-      </p>
-      {errorState.errors.length > 0 && errorState.continuePressed && (
-        <GenericError />
-      )}
+      </div>
+
+      {/* Show generic error message */}
+      {showGenericError}
+
       <fieldset className="wmnds-fe-fieldset">
         <legend className="wmnds-fe-fieldset__legend">
           <h2>What is your name?</h2>
@@ -37,20 +33,24 @@ const Step6Name = ({ setCurrentStep, formRef }) => {
         <Input
           className="wmnds-col-1 wmnds-col-sm-2-3 wmnds-col-lg-1-2"
           name="Firstname"
-          label="First name"
+          label={fNameLabel}
           autocomplete="given-name"
+          fieldValidation={fieldValidation(fNameLabel)}
         />
         <Input
           className="wmnds-col-1 wmnds-col-sm-2-3 wmnds-col-lg-1-2"
           name="Lastname"
-          label="Last name"
+          label={lNameLabel}
           autocomplete="family-name"
+          fieldValidation={fieldValidation(lNameLabel)}
         />
       </fieldset>
+
+      {/* Continue button */}
       <button
         type="button"
         className="wmnds-btn wmnds-btn--disabled wmnds-col-1 wmnds-m-t-md"
-        onClick={() => handleContinue()}
+        onClick={handleContinue}
       >
         Continue
       </button>
@@ -59,7 +59,6 @@ const Step6Name = ({ setCurrentStep, formRef }) => {
 };
 
 Step6Name.propTypes = {
-  setCurrentStep: PropTypes.func.isRequired,
   formRef: PropTypes.oneOfType([
     // Either a function
     PropTypes.func,
