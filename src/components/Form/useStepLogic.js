1e89f3a1b9ee1359c49af24fa629c40d03cbb2cc
Restyled.io <commits@restyled.io>
2020-05-12 08:06:09 +0000

    Restyled by clang-format

--- src/components/Form/useStepLogic.js
+++ src/components/Form/useStepLogic.js
@@ -1,24 +1,28 @@
-import React, { useState, useContext } from 'react';
-import { useFormContext } from 'react-hook-form';
-// Import contexts
-import { FormDataContext } from 'globalState/FormDataContext';
 // Import components
 import GenericError from 'components/shared/Errors/GenericError';
+// Import contexts
+import {FormDataContext} from 'globalState/FormDataContext';
+import React, {useContext, useState} from 'react';
+import {useFormContext} from 'react-hook-form';
 
 const useStepLogic = (formRef) => {
-  const { register, errors, triggerValidation, getValues } = useFormContext(); // Get useForm methods
-  const [formDataState, formDataDispatch] = useContext(FormDataContext); // Get the state/dispatch of form data from FormDataContext
-  const [isContinuePressed, setIsContinuePressed] = useState(false); // State for tracking if continue has been pressed
+  const {register, errors, triggerValidation, getValues} =
+      useFormContext(); // Get useForm methods
+  const [formDataState, formDataDispatch] =
+      useContext(FormDataContext); // Get the state/dispatch of form data from
+                                   // FormDataContext
+  const [isContinuePressed, setIsContinuePressed] =
+      useState(false); // State for tracking if continue has been pressed
   // Update the current step to the correct one depending on users selection
   const handleContinue = async () => {
     const result = await triggerValidation();
     setIsContinuePressed(true);
     // if no errors
     if (result) {
-      formDataDispatch({ type: 'UPDATE_FORM_DATA', payload: getValues() });
+      formDataDispatch({type : 'UPDATE_FORM_DATA', payload : getValues()});
       formDataDispatch({
-        type: 'UPDATE_STEP',
-        payload: formDataState.currentStep + 1,
+        type : 'UPDATE_STEP',
+        payload : formDataState.currentStep + 1,
       });
     }
     // else, errors are true...
@@ -27,9 +31,10 @@ const useStepLogic = (formRef) => {
     }
   };
 
-  // If errors object has any keys and continue button is pressed then we should show generic error component
-  const showGenericError = Object.keys(errors).length > 0 &&
-    isContinuePressed && <GenericError />;
+  // If errors object has any keys and continue button is pressed then we should
+  // show generic error component
+  const showGenericError =
+      Object.keys(errors).length > 0 && isContinuePressed && <GenericError />;
 
   return {
     register,
