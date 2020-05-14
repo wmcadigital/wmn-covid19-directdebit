ee1140d28ea5f88581ce57b51bb17954a784efb8
MJP <Martin.Price@wmca.org.uk>
2020-05-14 16:43:10 +0100

    Dear lord, more wizardry?

--- src/components/Form/Step9DDPayMessage/Step9DDPayMessage.js
+++ src/components/Form/Step9DDPayMessage/Step9DDPayMessage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'fecha';
 // Import contexts
 import { FormDataContext } from 'globalState/FormDataContext';
 // Import custom hooks
