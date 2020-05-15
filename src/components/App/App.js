f5fbdab2e66e9b85a9a42495357a47a1424adfe6
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-05-15 10:11:15 +0100

    Moved header and breadcumb into app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,26 +6,30 @@ import Form from 'components/Form/Form';
 import Introduction from 'components/Introduction/Introduction';
 import SuccessPage from 'components/SuccessPage/SuccessPage';
 import ErrorPage from 'components/ErrorPage/ErrorPage';
+import HeaderAndBreadcrumb from 'components/HeaderAndBreadcrumb/HeaderAndBreadcrumb';
 
 function App() {
   const [isFormStarted, setIsFormStarted] = useState(false);
   const [formSubmitStatus, setFormSubmitStatus] = useState(null);
 
   return (
-    <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
-      {/* If form isn't started, show intro...else show form */}
-      {!isFormStarted && <Introduction setIsFormStarted={setIsFormStarted} />}
+    <>
+      <HeaderAndBreadcrumb />
+      <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
+        {/* If form isn't started, show intro...else show form */}
+        {!isFormStarted && <Introduction setIsFormStarted={setIsFormStarted} />}
 
-      <FormDataProvider>
-        {isFormStarted && formSubmitStatus === null && (
-          <Form setFormSubmitStatus={setFormSubmitStatus} />
-        )}
+        <FormDataProvider>
+          {isFormStarted && formSubmitStatus === null && (
+            <Form setFormSubmitStatus={setFormSubmitStatus} />
+          )}
 
-        {formSubmitStatus && <SuccessPage />}
+          {formSubmitStatus && <SuccessPage />}
 
-        {formSubmitStatus === false && <ErrorPage />}
-      </FormDataProvider>
-    </div>
+          {formSubmitStatus === false && <ErrorPage />}
+        </FormDataProvider>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/HeaderAndBreadcrumb/HeaderAndBreadcrumb.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+const HeaderAndFooter = () => {
+  return (
+    <>
+      {/* <!-- Header --> */}
+      <header className="wmnds-header wmnds-header--mobile-app">
+        <div className="wmnds-container wmnds-grid wmnds-grid--align-center wmnds-grid--justify-between">
+          <div className="wmnds-header__vertical-align wmnds-col-auto">
+            {/* <!-- Logo --> */}
+            <a
+              className="wmnds-header__logo-link"
+              href="//wmnetwork.co.uk"
+              title="West Midlands Network Home"
+            >
+              <img
+                className="wmnds-header__logo"
+                alt="West Midlands Network logo"
+                src="//wmnetwork.netlify.com/img/logo.svg"
+              />
+            </a>
+            {/* <!-- Phase indicator --> */}
+            <a
+              href="//wmnetwork.co.uk"
+              target="_self"
+              className="wmnds-phase-indicator"
+              title="Beta homepage of West Midlands Network Home"
+            >
+              Beta
+            </a>
+          </div>
+
+          <h1 className="wmnds-header__title wmnds-col-1 wmnds-col-sm-auto">
+            Reinstate your Direct Debit <br />
+            (COVID-19)
+          </h1>
+        </div>
+      </header>
+      {/* <!-- End header --> */}
+
+      {/* <!-- BETA INFO--> */}
+      <div className="wmnds-container">
+        <div className="wmnds-grid wmnds-banner-container">
+          <div className="wmnds-col-auto wmnds-float-left wmnds-m-r-xsm">
+            <a
+              href="/"
+              target="_self"
+              className="wmnds-phase-indicator"
+              title="Beta homepage of West Midlands Network Design System"
+            >
+              Beta
+            </a>
+          </div>
+          <div className="wmnds-col-auto">
+            <p className="wmnds-banner-container__text">
+              This is a new service - your
+              <a
+                href="https://surveys.hotjar.com/s?siteId=264586&surveyId=156448"
+                title="Service feedback survey"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="wmnds-link"
+              >
+                feedback
+              </a>
+              will help us to improve it.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <!-- END BETA INFO--> */}
+
+      <div className="wmnds-container">
+        {/* <!-- Breadcrumb --> */}
+        <nav aria-label="Breadcrumb" className="wmnds-breadcrumb">
+          <ol className="wmnds-breadcrumb__list">
+            <li className="wmnds-breadcrumb__list-item">
+              <a href="//wmnetwork.co.uk" className="wmnds-breadcrumb__link">
+                Home
+              </a>
+            </li>
+            <li className="wmnds-breadcrumb__list-item">
+              <a
+                href="//www.wmnetwork.co.uk/tickets-and-passes/"
+                className="wmnds-breadcrumb__link"
+              >
+                Tickets and passes
+              </a>
+            </li>
+            <li className="wmnds-breadcrumb__list-item">
+              <a
+                href="//www.wmnetwork.co.uk/tickets-and-passes/direct-debit/"
+                className="wmnds-breadcrumb__link"
+              >
+                Direct Debit
+              </a>
+            </li>
+            <li className="wmnds-breadcrumb__list-item">
+              <a
+                href="/"
+                className="wmnds-breadcrumb__link wmnds-breadcrumb__link--current"
+                aria-current="page"
+              >
+                Reinstate your Direct Debit (COVID-19)
+              </a>
+            </li>
+          </ol>
+        </nav>
+        {/* <!-- End Breadcrumb --> */}
+      </div>
+    </>
+  );
+};
+
+export default HeaderAndFooter;
