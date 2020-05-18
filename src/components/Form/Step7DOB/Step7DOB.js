8b34b52ffd7fa01945f929230a73ae2a340145ff
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-05-18 10:15:12 +0100

    Added "can't be in future" DOB validation

--- src/components/Form/Step7DOB/Step7DOB.js
+++ src/components/Form/Step7DOB/Step7DOB.js
@@ -4,7 +4,7 @@ import React, { useRef } from 'react';
 import useStepLogic from 'components/Form/useStepLogic';
 import useWho from 'customHooks/useWho';
 // Import components
-import Date from 'components/shared/FormElements/Date/Date';
+import DateEle from 'components/shared/FormElements/Date/Date';
 
 const Step7DOB = () => {
   const formRef = useRef(); // Used so we can keep track of the form DOM element
@@ -19,13 +19,22 @@ const Step7DOB = () => {
   const DOBLabel = 'Date of birth'; // Label used on input and for validation
 
   // Logic used to validate the field
+
   const dateRegex = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/; // Date regex http://regexlib.com/REDetails.aspx?regexp_id=1850
+  const d = new Date().toISOString().slice(0, 10); // Set todays date as yyyy-mm-dd
 
   const fieldValidation = register({
     required: `${DOBLabel} is required`,
     pattern: {
       value: dateRegex,
       message: `Enter ${DOBLabel.toLowerCase()} in the correct format, for example 18 3 1998`,
+    },
+    validate: {
+      DOBmustBeInPast: (val) =>
+        val < d ||
+        `${
+          yourTheir.charAt(0).toUpperCase() + yourTheir.slice(1)
+        } date of birth can't be in the future.`,
     },
   });
 
@@ -45,7 +54,7 @@ const Step7DOB = () => {
         </legend>
         <p>We&apos;ll use this information to confirm {yourTheir} identity</p>
         <p>For example, 31 3 1980</p>
-        <Date
+        <DateEle
           name="DateOfBirth"
           label={DOBLabel}
           autoCompletPrefix="bday-"
